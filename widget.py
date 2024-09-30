87e9ebe6c6d6f3501bccf0cd515c238622688633
Marco Coppola <38087157+Murkrow02@users.noreply.github.com>
2024-09-30 20:34:06 +0200

    Update widget.py

--- widget.py
+++ widget.py
@@ -4,7 +4,7 @@ from mosaico import widget, config
 text = widget.createText()
 text.setText(config["name"])
 text.setHexColor(config["color"])
-text.moveTo(3,0)
+text.moveTo(2,0)
 text.setFont("9x18")
 
 # Create items
@@ -14,7 +14,7 @@ for i in range(0, len(config["items"])):
     # Create bullet
     bullets.append(widget.createRectangle())
     bullets[i].setSize(2,2)        
-    bullets[i].moveTo(4,14)
+    bullets[i].moveTo(4,16)
     bullets[i].translateYBy((i*7) + 5)    
     bullets[i].setHexColor(config["color"])  
     
@@ -22,7 +22,7 @@ for i in range(0, len(config["items"])):
     items.append(widget.createText())
     items[i].setFont("4x6")
     items[i].setText(config["items"][i])
-    items[i].moveTo(8,12)    
+    items[i].moveTo(8,14)    
     items[i].translateYBy((i*7) + 5)
 
 def loop():
